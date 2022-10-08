724301ad56bc6c38525e698b9d6a1aa72677cc9f
lpuls <linepuls@gmail.com>
2022-10-08 17:44:53 +0800

    提交meta文件

---
+++ Assets/Scripts/Component/Enemy/BaseEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Hamster.SpaceWar {
+
+    public class BaseEnemy : BaseController {
+
+        protected MovementComponent _movementComponent = null;
+        protected LocalAbilityComponent _localAbilityComponent = null;
+
+        public override void Init() {
+            base.Init();
+            _movementComponent = GetComponent<MovementComponent>();
+            _localAbilityComponent = GetComponent<LocalAbilityComponent>();
+        }
+
+        public override void Tick(float dt) {
+            base.Tick(dt);
+
+        }
+
+    }
+}
