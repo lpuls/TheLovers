80dc8ee5c2f5151514bd36d08574964437efb370
lpuls <linepuls@gmail.com>
2022-10-13 22:51:26 +0800

    实现敌人生成器的配置文件及编辑器

---
+++ Assets/Scripts/Component/Enemy/Debuger/AIEnemeySpawnDataDebuger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hamster.SpaceWar {
+
+#if UNITY_EDITOR
+    public class AIEnemeySpawnDataDebuger : BaseEnemeySpawnDataDebuger {
+        public List<int> SpawnIDs = new List<int>();
+
+        public int AIEnemeyMinSpawnCount = 1;
+        public int AIEnemeyMaxSpawnCount = 5;
+
+        public override EnemySpawnData GetEnemySpawnData() {
+            AIEnemySpawnData enemySpawnData = new AIEnemySpawnData();
+            enemySpawnData.RandomMinCount = AIEnemeyMinSpawnCount;
+            enemySpawnData.RandomMaxCount = AIEnemeyMaxSpawnCount;
+            enemySpawnData.SpawnIDs = SpawnIDs;
+            enemySpawnData.TriggerZ = transform.position.z;
+            return enemySpawnData;
+        }
+    }
+#endif
+}
---
+++ Assets/Scripts/ScriptObjects/EnemySpawnDatas.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hamster.SpaceWar {
+
+    [System.Serializable]
+    public class EnemySpawnData {
+        public float TriggerZ = 0;
+        public List<int> SpawnIDs = new List<int>(16);
+    }
+
+    [System.Serializable]
+    public class AIEnemySpawnData : EnemySpawnData {
+        public bool IsRandom = false;
+
+        public int RandomMinCount = 1;
+        public int RandomMaxCount = 5;
+
+        public List<Vector3> SpawnLocations = new List<Vector3>(16);
+    }
+
+    [CreateAssetMenu(menuName = "ScriptObject/Enemy/Enemy Spawn")]
+    public class EnemySpawnDatas : ScriptableObject {
+        public List<EnemySpawnData> Datas = new List<EnemySpawnData>(); 
+    }
+}
