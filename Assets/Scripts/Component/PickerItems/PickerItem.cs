2bff94cc99cf5d2add2f34fe32c96b05f32d2e33
lpuls <linepuls@gmail.com>
2022-10-23 21:01:34 +0800

    添加道具拾取的基类和武器拾取

---
+++ Assets/Scripts/Component/PickerItems/PickerItem.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Hamster.SpaceWar {
+    public class PickerItem : BaseController {
+
+        [SerializeField] private float _moveSpeed = 10;
+        [SerializeField] private float _itemSize = 1.0f;
+        private Vector3 _moveDirection = Vector3.zero;
+
+        public override void OnEnable() {
+            base.OnEnable();
+            _moveDirection = GetRandomDirection();
+        }
+
+        public override void Tick(float dt) {
+            base.Tick(dt);
+
+            float moveDistance = dt * _moveSpeed;
+            if (World.GetWorld<BaseSpaceWarWorld>().InWorld(transform.position, _moveDirection * moveDistance, out moveDistance)) {
+
+                RaycastHit2D hitResult = Physics2D.BoxCast(transform.position, new Vector2(_itemSize, _itemSize), 0, _moveDirection, moveDistance, 1 << (int)ESpaceWarLayers.PLAYER);
+                if (null != hitResult.collider && hitResult.collider.gameObject.TryGetComponent<PlayerController>(out PlayerController playerController)) {
+                    OnPicker(playerController);
+                    AssetPool.Free(gameObject);
+                }
+
+                if (moveDistance <= _itemSize) {
+                    _moveDirection = GetRandomDirection();
+                }
+            }
+
+        }
+
+        protected static Vector3 GetRandomDirection() {
+            float x = Random.Range(0, 1.0f);
+            float y = Random.Range(0, 1.0f);
+            return (new Vector3(x, y)).normalized;
+        } 
+
+        protected virtual void OnPicker(PlayerController playerController) {
+            throw new System.NotImplementedException();
+        }
+
+#if UNITY_EDITOR
+        public void OnDrawGizmos() {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, _itemSize);
+        }
+#endif
+
+    }
+}
---
+++ Assets/Scripts/Component/PickerItems/WeaponPickerItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Hamster.SpaceWar {
+    public class WeaponPickerItem : PickerItem {
+        public EAbilityIndex AbilityIndex = EAbilityIndex.Fire;
+        public int WeaponID = 0;
+
+        protected override void OnPicker(PlayerController playerController) {
+            base.OnPicker(playerController);
+
+            ServerPlayerController serverPlayerController = playerController as ServerPlayerController;
+            if (null != serverPlayerController) {
+                serverPlayerController.ChangeWeapon(AbilityIndex, WeaponID);
+            }
+        }
+    }
+}
