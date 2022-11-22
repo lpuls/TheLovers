51910cf7179c64e861d8ee9b7e8495d383cc886c
lpuls <linepuls@gmail.com>
2022-11-22 15:37:53 +0800

    整理资源配置文件

---
+++ Assets/Scripts/ScriptObjects/LevelEditor/LevelEventScriptObject.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hamster.SpaceWar {
+    public enum ELevelWaveCompleteType {
+        WaitTime,
+        WaitAllDie,
+    }
+
+    public interface ILevelManager {
+        bool IsServer();
+        float GetTime();
+        int GetEnemeyCount();
+        void SpawnUnit(int id, int locationIndex);
+        void DestroyAllUnit();
+
+    }
+
+    [SerializeField]
+    public class LevelEventScriptObject : ScriptableObject {
+        public float Time = 0;
+
+        public virtual bool IsComplete(ILevelManager levelManager) {
+            return levelManager.GetTime() >= Time;
+        }
+
+        public virtual void OnLevel(ILevelManager levelManager) {
+        }
+
+        public virtual void OnEnter(ILevelManager levelManager) {
+        }
+
+#if UNITY_EDITOR
+        public virtual void Save(ScriptableObject parent) {
+            UnityEditor.AssetDatabase.AddObjectToAsset(this, parent);
+            UnityEditor.EditorUtility.SetDirty(this);
+            UnityEditor.AssetDatabase.SaveAssets();
+        }
+#endif
+    }
+
+    [SerializeField]
+    public class LevelWaveScriptObject : LevelEventScriptObject {
+
+        public ELevelWaveCompleteType CompleteType = ELevelWaveCompleteType.WaitTime;
+        public List<UnitSpawnScriptObject> UnitSpawns = new();  // 敌人生成数据
+
+        public override bool IsComplete(ILevelManager levelManager) {
+            if (ELevelWaveCompleteType.WaitTime == CompleteType) {
+                return levelManager.GetTime() >= Time;
+            }
+            else if (ELevelWaveCompleteType.WaitAllDie == CompleteType) {
+                return levelManager.GetEnemeyCount() <= 0;
+            }
+            return false;
+        }
+
+        public override void OnLevel(ILevelManager levelManager) {
+            base.OnLevel(levelManager);
+        }
+
+        public override void OnEnter(ILevelManager levelManager) {
+            if (!levelManager.IsServer())
+                return;
+            foreach (var item in UnitSpawns) {
+                levelManager.SpawnUnit(item.ID, item.LocationIndex);
+            }
+        }
+
+#if UNITY_EDITOR
+        public override void Save(ScriptableObject parent) {
+            base.Save(parent);
+
+            foreach (var item in UnitSpawns) {
+                item.Save(this);
+            }
+
+        }
+#endif 
+    }
+
+    [SerializeField]
+    public class LevelMissionUIScriptObject : LevelEventScriptObject {
+        public int MissionID = 0;
+
+        public override void OnEnter(ILevelManager levelManager) {
+            if (levelManager.IsServer())
+                return;
+            Single<UIManager>.GetInstance().Open<MissonUIController>();
+            MissionUIModule missionUIModule = Single<UIManager>.GetInstance().GetModule<MissonUIController>() as MissionUIModule;
+            missionUIModule.MissionID.SetValue(MissionID);
+        }
+
+        public override void OnLevel(ILevelManager levelManager) {
+            if (levelManager.IsServer())
+                return;
+            Single<UIManager>.GetInstance().Close<MissonUIController>();
+        }
+    }
+}
